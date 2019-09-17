1d8c5077df5d666e111181bc7ff8c9c00f5df194
ForForeveryu <15518812818@163.com>
2019-09-17 13:03:47 +0800

    新增一个Single.java

--- Single.java
+++ Single.java
@@ -1,7 +1,7 @@
  class Single{
    private static Single INSTANCE=new Single();
    private Single() {}
-   public static Single getInstance() {
+    public static Single getInstance() {
      return INSTANCE;
 }
 }
